306f737167c046fcd8df45dcca641d1df5af3c5f
RDawlekar <rkumartippiripati@gmail.com>
2023-10-14 22:30:16 +0530

    commit1

---
+++ BrowserLogin.java
@@ -0,0 +1,11 @@
+public class BrowserLogin {
+
+    public static void main()
+    {
+        System.setProperty("Driver.chrome.driver","C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\chrome.exe");
+        System.out.println("hellow");
+        //        WebDriver driver=new ChromeDriver();
+
+    }
+
+}
